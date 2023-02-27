494b4ba679731c90457a6d5898ebe0ab3b7dedaa
ViStef1337 <stefanykvitalik1337@gmail.com>
2023-02-27 11:15:05 -0600

    preproc

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,13 @@ $(document).ready(function(){
 
 
 })(jQuery);
+(function () {
+    const header = document.querySelector('header');
+    window.onscroll = () => {
+        if (window.pageYOffset> 50){
+            header.classList.add('header_active')
+        } else {
+            header.classList.remove('header_active')
+        }
+    };
+}())
